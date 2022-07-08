497f8ec0df7b14d8bae57fc211ccf89ac84523cd
Michał Postek <postek.michal@protonmail.com>
2022-07-08 14:42:52 +0200

    show code snippet to user

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const flexbox = document.querySelector('.generator__illustration');
 const valueSelects = document.querySelectorAll('.value-select');
+const rulesOutput = document.querySelector('.rules-output');
 
 valueSelects.forEach((select) => {
     select.addEventListener('change', () => {
         const { dataset: { property }, value } = select;
-        
         flexbox.style[property] = value;
+
+        showCode();
     });
 });
+
+const showCode = () => {
+    rulesOutput.innerHTML = '';
+
+    valueSelects.forEach((select) => {
+        const { dataset: { property }, value } = select;
+        const rule = `<span>${property}: ${value};<span>`;
+
+        rulesOutput.innerHTML += rule;
+    });
+}
+
+showCode();
