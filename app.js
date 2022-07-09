98370800ec066023b3922aadd36a0b0098ea5344
Michał Postek <postek.michal@protonmail.com>
2022-07-09 11:55:49 +0200

    add button for copying the code snippet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const flexbox = document.querySelector('.generator__preview');
 const valueSelects = document.querySelectorAll('.value-select');
 const codeOutput = document.querySelector('.generator__code');
+const codeRules = document.querySelector('.code__rules');
+const copyCodeBtn = document.querySelector('#copyCodeBtn');
 
 valueSelects.forEach((select) => {
     select.addEventListener('change', () => {
@@ -11,14 +13,20 @@ valueSelects.forEach((select) => {
     });
 });
 
+copyCodeBtn.addEventListener('click', () => {
+    const code = codeOutput.textContent;
+
+    navigator.clipboard.writeText(code);
+});
+
 const showCode = () => {
-    codeOutput.innerHTML = '';
+    codeRules.innerHTML = '';
 
     valueSelects.forEach((select) => {
         const { value, name } = select;
         const rule = `<span>${name}: ${value};<span>`;
 
-        codeOutput.innerHTML += rule;
+        codeRules.innerHTML += rule;
     });
 }
 
