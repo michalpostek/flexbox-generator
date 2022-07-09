9ff8ed6e0004386e1787900d82d5e59dde7d38df
Michał Postek <postek.michal@protonmail.com>
2022-07-09 14:57:47 +0200

    add button animation on click event

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const codeOutput = document.querySelector('.generator__code');
 const codeRules = document.querySelector('.code__rules');
 const copyCodeBtn = document.querySelector('#copyCodeBtn');
 
+const buttonAnimation = [
+    {}, {
+        background: 'var(--beige)', 
+        color: 'var(--navy)'
+    }, {}
+];
+
+const buttonTiming = {
+    duration: 500,
+    iterations: 1,
+}
+
 valueSelects.forEach((select) => {
     select.addEventListener('change', () => {
         const { dataset: { property }, value } = select;
@@ -15,8 +27,9 @@ valueSelects.forEach((select) => {
 
 copyCodeBtn.addEventListener('click', () => {
     const code = codeOutput.textContent;
-
     navigator.clipboard.writeText(code);
+
+    copyCodeBtn.animate(buttonAnimation, buttonTiming);
 });
 
 const showCode = () => {
